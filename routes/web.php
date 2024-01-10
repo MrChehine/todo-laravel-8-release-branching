7936958268ab06f1fe0429cfc14d9e38444db053
Chehine Ammari <chehine.ammari@gmail.com>
2024-01-10 21:22:10 +0100

    Omit comment on web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [TodoController::class,'index'])->name('index');
 Route::post('/', [TodoController::class,'store'])->name('store');
 Route::patch('/{todo}', [TodoController::class,'update'])->name('update');
